78c065165b252e30b705f3b7af418c13a3176325
saravana <saravanasaru14313@gmail.com>
2019-02-11 23:27:27 -0800

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class Multiple 
+{
+  public static void main(String args[])
+  {
+        int a;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+	if(a%7==0)
+	{
+		System.out.println("Yes");
+	}
+	else
+	{
+		System.out.println("No");
+	}
+  }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Multipleof7
